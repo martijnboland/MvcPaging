a70029d828f3224bf4f6069c0ac324e507205659
cvrajeesh <cvrajeesh@gmail.com>
2012-04-17 22:38:13 +0530

    Support for Pagination model so that user can build customized pager views

---
+++ src/MvcPaging/PaginationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcPaging
+{  
+    public class PaginationModel
+    {
+        public bool Active { get; set; }
+
+        public bool IsCurrent { get; set; }
+
+        public int? PageIndex { get; set; }
+
+        public string DisplayText { get; set; }
+
+    }
+}
