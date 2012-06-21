cb4b19f170634bded653a482436bb5ee32be02b4
Martijn Boland <martijn@boland.org>
2012-06-21 21:38:52 +0200

    Added comment for PagerOptions.Defaults. Formatting: spaces -> tabs.

--- src/MvcPaging.Tests/PagerTests.cs
+++ src/MvcPaging.Tests/PagerTests.cs
@@ -43,31 +43,31 @@ namespace MvcPaging.Tests
 			}
 		}
 
-        [Test]
-	    public void Can_Defaults_Be_Set()
-        {
-            var first = new PagerOptions();
-            PagerOptions.Defaults.AlwaysAddFirstPageNumber = !PagerOptions.DefaultDefaults.AlwaysAddFirstPageNumber;
-            PagerOptions.Defaults.DisplayTemplate = PagerOptions.DefaultDefaults.DisplayTemplate + "-test";
-            PagerOptions.Defaults.MaxNrOfPages = PagerOptions.DefaultDefaults.MaxNrOfPages + 1;
+		[Test]
+		public void Can_Defaults_Be_Set()
+		{
+			var first = new PagerOptions();
+			PagerOptions.Defaults.AlwaysAddFirstPageNumber = !PagerOptions.DefaultDefaults.AlwaysAddFirstPageNumber;
+			PagerOptions.Defaults.DisplayTemplate = PagerOptions.DefaultDefaults.DisplayTemplate + "-test";
+			PagerOptions.Defaults.MaxNrOfPages = PagerOptions.DefaultDefaults.MaxNrOfPages + 1;
 
-            var second = new PagerOptions();
+			var second = new PagerOptions();
 
-            Assert.AreEqual(second.AlwaysAddFirstPageNumber, PagerOptions.Defaults.AlwaysAddFirstPageNumber);
-            Assert.AreEqual(second.DisplayTemplate, PagerOptions.Defaults.DisplayTemplate);
-            Assert.AreEqual(second.MaxNrOfPages, PagerOptions.Defaults.MaxNrOfPages);
+			Assert.AreEqual(second.AlwaysAddFirstPageNumber, PagerOptions.Defaults.AlwaysAddFirstPageNumber);
+			Assert.AreEqual(second.DisplayTemplate, PagerOptions.Defaults.DisplayTemplate);
+			Assert.AreEqual(second.MaxNrOfPages, PagerOptions.Defaults.MaxNrOfPages);
 
-            Assert.AreNotEqual(first.AlwaysAddFirstPageNumber, second.AlwaysAddFirstPageNumber);
-            Assert.AreNotEqual(first.DisplayTemplate, second.DisplayTemplate);
-            Assert.AreNotEqual(first.MaxNrOfPages, second.MaxNrOfPages);
+			Assert.AreNotEqual(first.AlwaysAddFirstPageNumber, second.AlwaysAddFirstPageNumber);
+			Assert.AreNotEqual(first.DisplayTemplate, second.DisplayTemplate);
+			Assert.AreNotEqual(first.MaxNrOfPages, second.MaxNrOfPages);
 
-            // cleanup
-            PagerOptions.Defaults.ResetToDefaults();
+			// cleanup
+			PagerOptions.Defaults.Reset();
 
-            Assert.AreEqual(PagerOptions.DefaultDefaults.AlwaysAddFirstPageNumber, PagerOptions.Defaults.AlwaysAddFirstPageNumber);
-            Assert.AreEqual(PagerOptions.DefaultDefaults.DisplayTemplate, PagerOptions.Defaults.DisplayTemplate);
-            Assert.AreEqual(PagerOptions.DefaultDefaults.MaxNrOfPages, PagerOptions.Defaults.MaxNrOfPages);
-        }
+			Assert.AreEqual(PagerOptions.DefaultDefaults.AlwaysAddFirstPageNumber, PagerOptions.Defaults.AlwaysAddFirstPageNumber);
+			Assert.AreEqual(PagerOptions.DefaultDefaults.DisplayTemplate, PagerOptions.Defaults.DisplayTemplate);
+			Assert.AreEqual(PagerOptions.DefaultDefaults.MaxNrOfPages, PagerOptions.Defaults.MaxNrOfPages);
+		}
 
 		[Test]
 		public void Can_Action_Be_Set_Before_RouteData()
--- src/MvcPaging/PagerOptions.cs
+++ src/MvcPaging/PagerOptions.cs
@@ -5,29 +5,35 @@ namespace MvcPaging
 {
 	public class PagerOptions
 	{
-        public static class DefaultDefaults
-        {
-            public const int MaxNrOfPages = 10;
-            public const string DisplayTemplate = null;
-            public const bool AlwaysAddFirstPageNumber = false;
-        }
-		const string DefaultPageRouteValueKey = "page";
+		public static class DefaultDefaults
+		{
+			public const int MaxNrOfPages = 10;
+			public const string DisplayTemplate = null;
+			public const bool AlwaysAddFirstPageNumber = false;
+			public const string DefaultPageRouteValueKey = "page";
+		}
 
-        public static class Defaults
-        {
-            public static int MaxNrOfPages = DefaultDefaults.MaxNrOfPages;
-            public static string DisplayTemplate = DefaultDefaults.DisplayTemplate;
-            public static bool AlwaysAddFirstPageNumber = DefaultDefaults.AlwaysAddFirstPageNumber;
+		/// <summary>
+		/// The static Defaults class allows you to set Pager defaults for the entire application. 
+		/// Set values at application startup.
+		/// </summary>
+		public static class Defaults
+		{
+			public static int MaxNrOfPages = DefaultDefaults.MaxNrOfPages;
+			public static string DisplayTemplate = DefaultDefaults.DisplayTemplate;
+			public static bool AlwaysAddFirstPageNumber = DefaultDefaults.AlwaysAddFirstPageNumber;
+			public static string DefaultPageRouteValueKey = DefaultDefaults.DefaultPageRouteValueKey;
 
-            public static void ResetToDefaults()
-            {
-                MaxNrOfPages = DefaultDefaults.MaxNrOfPages;
-                DisplayTemplate = DefaultDefaults.DisplayTemplate;
-                AlwaysAddFirstPageNumber = DefaultDefaults.AlwaysAddFirstPageNumber;
-            }
-        }
+			public static void Reset()
+			{
+				MaxNrOfPages = DefaultDefaults.MaxNrOfPages;
+				DisplayTemplate = DefaultDefaults.DisplayTemplate;
+				AlwaysAddFirstPageNumber = DefaultDefaults.AlwaysAddFirstPageNumber;
+				DefaultPageRouteValueKey = DefaultDefaults.DefaultPageRouteValueKey;
+			}
+		}
 
-        public RouteValueDictionary RouteValues { get; internal set; }
+		public RouteValueDictionary RouteValues { get; internal set; }
 		public string DisplayTemplate { get; internal set; }
 		public int MaxNrOfPages { get; internal set; }
 		public AjaxOptions AjaxOptions { get; internal set; }
@@ -38,10 +44,10 @@ namespace MvcPaging
 		public PagerOptions()
 		{
 			RouteValues = new RouteValueDictionary();
-            DisplayTemplate = Defaults.DisplayTemplate;
-            MaxNrOfPages = Defaults.MaxNrOfPages;
-            AlwaysAddFirstPageNumber = Defaults.AlwaysAddFirstPageNumber;
-			PageRouteValueKey = DefaultPageRouteValueKey;
+			DisplayTemplate = Defaults.DisplayTemplate;
+			MaxNrOfPages = Defaults.MaxNrOfPages;
+			AlwaysAddFirstPageNumber = Defaults.AlwaysAddFirstPageNumber;
+			PageRouteValueKey = Defaults.DefaultPageRouteValueKey;
 		}
 	}
 }
