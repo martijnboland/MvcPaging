50a997788f5be6bcd98832023d6434e2a7302e66
Nicollas Ricas <nicollasfernandesricas@gmail.com>
2014-07-10 10:35:52 -0400

    Added support for display first and last pages and custom display title on next/previous
    Example of usage:
    
    @Html.Pager(...).Options(o => o.SetPreviousPageTitle("Previous
    page").DisplayFirstAndLastPage().SetFirstPageTitle("Go to first page"))

--- src/MvcPaging/Pager.cs
+++ src/MvcPaging/Pager.cs
@@ -7,219 +7,200 @@ using System.Web.Routing;
 
 namespace MvcPaging
 {
-	public class Pager : IHtmlString
-	{
-		private readonly HtmlHelper htmlHelper;
-		private readonly int pageSize;
-		private readonly int currentPage;
-		private readonly int totalItemCount;
-		protected readonly PagerOptions pagerOptions;
+    public class Pager : IHtmlString
+    {
+        private readonly HtmlHelper htmlHelper;
+        private readonly int pageSize;
+        private readonly int currentPage;
+        private readonly int totalItemCount;
+        protected readonly PagerOptions pagerOptions;
 
-		public Pager(HtmlHelper htmlHelper, int pageSize, int currentPage, int totalItemCount)
-		{
-			this.htmlHelper = htmlHelper;
-			this.pageSize = pageSize;
-			this.currentPage = currentPage;
-			this.totalItemCount = totalItemCount;
-			this.pagerOptions = new PagerOptions();
-		}
+        public Pager(HtmlHelper htmlHelper, int pageSize, int currentPage, int totalItemCount)
+        {
+            this.htmlHelper = htmlHelper;
+            this.pageSize = pageSize;
+            this.currentPage = currentPage;
+            this.totalItemCount = totalItemCount;
+            this.pagerOptions = new PagerOptions();
+        }
 
-		public Pager Options(Action<PagerOptionsBuilder> buildOptions)
-		{
-			buildOptions(new PagerOptionsBuilder(this.pagerOptions));
-			return this;
-		}
+        public Pager Options(Action<PagerOptionsBuilder> buildOptions)
+        {
+            buildOptions(new PagerOptionsBuilder(this.pagerOptions));
+            return this;
+        }
 
-		public virtual PaginationModel BuildPaginationModel(Func<int, string> generateUrl)
-		{
-			var pageCount = (int)Math.Ceiling(totalItemCount / (double)pageSize);
-			var model = new PaginationModel { PageSize = this.pageSize, CurrentPage = this.currentPage, TotalItemCount = this.totalItemCount, PageCount = pageCount };
+        public virtual PaginationModel BuildPaginationModel(Func<int, string> generateUrl)
+        {
+            var pageCount = (int)Math.Ceiling(totalItemCount / (double)pageSize);
+            var model = new PaginationModel { PageSize = this.pageSize, CurrentPage = this.currentPage, TotalItemCount = this.totalItemCount, PageCount = pageCount };
 
-			var previousPageText = this.pagerOptions.PreviousPageText;
-			// Previous
-			model.PaginationLinks.Add(currentPage > 1 ? new PaginationLink { Active = true, DisplayText = previousPageText, PageIndex = currentPage - 1, Url = generateUrl(currentPage - 1) } : new PaginationLink { Active = false, DisplayText = previousPageText });
+            // First page
+            if (this.pagerOptions.DisplayFirstAndLastPage) {
+                model.PaginationLinks.Add(new PaginationLink { Active = (currentPage != 1 ? true : false), DisplayText = this.pagerOptions.FirstPageText, DisplayTitle = this.pagerOptions.FirstPageTitle, PageIndex = 1, Url = generateUrl(1) });
+            }
 
-			var start = 1;
-			var end = pageCount;
-			var nrOfPagesToDisplay = this.pagerOptions.MaxNrOfPages;
+            // Previous page
+            var previousPageText = this.pagerOptions.PreviousPageText;
+            model.PaginationLinks.Add(currentPage > 1 ? new PaginationLink { Active = true, DisplayText = previousPageText, DisplayTitle = this.pagerOptions.PreviousPageTitle, PageIndex = currentPage - 1, Url = generateUrl(currentPage - 1) } : new PaginationLink { Active = false, DisplayText = previousPageText });
 
-			if (pageCount > nrOfPagesToDisplay)
-			{
-				var middle = (int)Math.Ceiling(nrOfPagesToDisplay / 2d) - 1;
-				var below = (currentPage - middle);
-				var above = (currentPage + middle);
+            var start = 1;
+            var end = pageCount;
+            var nrOfPagesToDisplay = this.pagerOptions.MaxNrOfPages;
 
-				if (below < 2)
-				{
-					above = nrOfPagesToDisplay;
-					below = 1;
-				}
-				else if (above > (pageCount - 2))
-				{
-					above = pageCount;
-					below = (pageCount - nrOfPagesToDisplay + 1);
-				}
+            if (pageCount > nrOfPagesToDisplay) {
+                var middle = (int)Math.Ceiling(nrOfPagesToDisplay / 2d) - 1;
+                var below = (currentPage - middle);
+                var above = (currentPage + middle);
 
-				start = below;
-				end = above;
-			}
+                if (below < 2) {
+                    above = nrOfPagesToDisplay;
+                    below = 1;
+                } else if (above > (pageCount - 2)) {
+                    above = pageCount;
+                    below = (pageCount - nrOfPagesToDisplay + 1);
+                }
 
-			if (start > 1)
-			{
-				model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = 1, DisplayText = "1", Url = generateUrl(1) });
-				if (start > 3)
-				{
-					model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = 2, DisplayText = "2", Url = generateUrl(2) });
-				}
-				if (start > 2)
-				{
-					model.PaginationLinks.Add(new PaginationLink { Active = false, DisplayText = "...", IsSpacer = true });
-				}
-			}
+                start = below;
+                end = above;
+            }
 
-			for (var i = start; i <= end; i++)
-			{
-				if (i == currentPage || (currentPage <= 0 && i == 1))
-				{
-					model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = i, IsCurrent = true, DisplayText = i.ToString() });
-				}
-				else
-				{
-					model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = i, DisplayText = i.ToString(), Url = generateUrl(i) });
-				}
-			}
-			if (end < pageCount)
-			{
-				if (end < pageCount - 1)
-				{
-					model.PaginationLinks.Add(new PaginationLink { Active = false, DisplayText = "...", IsSpacer = true });
-				}
-				if (pageCount - 2 > end)
-				{
-					model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = pageCount - 1, DisplayText = (pageCount - 1).ToString(), Url = generateUrl(pageCount - 1) });
-				}
+            if (start > 1) {
+                model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = 1, DisplayText = "1", Url = generateUrl(1) });
+                if (start > 3) {
+                    model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = 2, DisplayText = "2", Url = generateUrl(2) });
+                }
+                if (start > 2) {
+                    model.PaginationLinks.Add(new PaginationLink { Active = false, DisplayText = "...", IsSpacer = true });
+                }
+            }
 
-				model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = pageCount, DisplayText = pageCount.ToString(), Url = generateUrl(pageCount) });
-			}
+            for (var i = start; i <= end; i++) {
+                if (i == currentPage || (currentPage <= 0 && i == 1)) {
+                    model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = i, IsCurrent = true, DisplayText = i.ToString() });
+                } else {
+                    model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = i, DisplayText = i.ToString(), Url = generateUrl(i) });
+                }
+            }
 
-			var nextPageText = this.pagerOptions.NextPageText;
-			// Next
-			model.PaginationLinks.Add(currentPage < pageCount ? new PaginationLink { Active = true, PageIndex = currentPage + 1, DisplayText = nextPageText, Url = generateUrl(currentPage + 1) } : new PaginationLink { Active = false, DisplayText = nextPageText });
+            if (end < pageCount) {
+                if (end < pageCount - 1) {
+                    model.PaginationLinks.Add(new PaginationLink { Active = false, DisplayText = "...", IsSpacer = true });
+                }
+                if (pageCount - 2 > end) {
+                    model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = pageCount - 1, DisplayText = (pageCount - 1).ToString(), Url = generateUrl(pageCount - 1) });
+                }
 
-			// AjaxOptions
-			if (pagerOptions.AjaxOptions != null)
-			{
-				model.AjaxOptions = pagerOptions.AjaxOptions;
-			}
+                model.PaginationLinks.Add(new PaginationLink { Active = true, PageIndex = pageCount, DisplayText = pageCount.ToString(), Url = generateUrl(pageCount) });
+            }
 
-			model.Options = pagerOptions;
-			return model;
-		}
+            // Next page
+            var nextPageText = this.pagerOptions.NextPageText;
+            model.PaginationLinks.Add(currentPage < pageCount ? new PaginationLink { Active = true, PageIndex = currentPage + 1, DisplayText = nextPageText, DisplayTitle = this.pagerOptions.NextPageTitle, Url = generateUrl(currentPage + 1) } : new PaginationLink { Active = false, DisplayText = nextPageText });
 
-		public virtual string ToHtmlString()
-		{
-			var model = BuildPaginationModel(GeneratePageUrl);
+            // Last page
+            if (this.pagerOptions.DisplayFirstAndLastPage) {
+                model.PaginationLinks.Add(new PaginationLink { Active = (currentPage != pageCount ? true : false), DisplayText = this.pagerOptions.LastPageText, DisplayTitle = this.pagerOptions.LastPageTitle, PageIndex = pageCount, Url = generateUrl(pageCount) });
+            }
 
-			if (!String.IsNullOrEmpty(this.pagerOptions.DisplayTemplate))
-			{
-				var templatePath = string.Format("DisplayTemplates/{0}", this.pagerOptions.DisplayTemplate);
-				return htmlHelper.Partial(templatePath, model).ToHtmlString();
-			}
-			else
-			{
-				var sb = new StringBuilder();
+            // AjaxOptions
+            if (pagerOptions.AjaxOptions != null) {
+                model.AjaxOptions = pagerOptions.AjaxOptions;
+            }
 
-				foreach (var paginationLink in model.PaginationLinks)
-				{
-					if (paginationLink.Active)
-					{
-						if (paginationLink.IsCurrent)
-						{
-							sb.AppendFormat("<span class=\"current\">{0}</span>", paginationLink.DisplayText);
-						}
-						else if (!paginationLink.PageIndex.HasValue)
-						{
-							sb.AppendFormat(paginationLink.DisplayText);
-						}
-						else
-						{
-							var linkBuilder = new StringBuilder("<a");
-							if (pagerOptions.AjaxOptions != null)
-								foreach (var ajaxOption in pagerOptions.AjaxOptions.ToUnobtrusiveHtmlAttributes())
-									linkBuilder.AppendFormat(" {0}=\"{1}\"", ajaxOption.Key, ajaxOption.Value);
+            model.Options = pagerOptions;
+            return model;
+        }
 
-							linkBuilder.AppendFormat(" href=\"{0}\">{1}</a>", paginationLink.Url, paginationLink.DisplayText);
-							sb.Append(linkBuilder.ToString());
-						}
-					}
-					else
-					{
-						if (!paginationLink.IsSpacer)
-						{
-							sb.AppendFormat("<span class=\"disabled\">{0}</span>", paginationLink.DisplayText);
-						}
-						else
-						{
-							sb.AppendFormat("<span class=\"spacer\">{0}</span>", paginationLink.DisplayText);
-						}
-					}
-				}
-				return sb.ToString();
-			}
-		}
+        public virtual string ToHtmlString()
+        {
+            var model = BuildPaginationModel(GeneratePageUrl);
 
-		protected virtual string GeneratePageUrl(int pageNumber)
-		{
-			var viewContext = this.htmlHelper.ViewContext;
-			var routeDataValues = viewContext.RequestContext.RouteData.Values;
-			RouteValueDictionary pageLinkValueDictionary;
-			// Avoid canonical errors when pageNumber is equal to 1.
-			if (pageNumber == 1 && !this.pagerOptions.AlwaysAddFirstPageNumber)
-			{
-				pageLinkValueDictionary = new RouteValueDictionary(this.pagerOptions.RouteValues);
-				if (routeDataValues.ContainsKey(this.pagerOptions.PageRouteValueKey))
-				{
-					routeDataValues.Remove(this.pagerOptions.PageRouteValueKey);
-				}
-			}
-			else
-			{
-				pageLinkValueDictionary = new RouteValueDictionary(this.pagerOptions.RouteValues) { { this.pagerOptions.PageRouteValueKey, pageNumber } };
-			}
+            if (!String.IsNullOrEmpty(this.pagerOptions.DisplayTemplate)) {
+                var templatePath = string.Format("DisplayTemplates/{0}", this.pagerOptions.DisplayTemplate);
+                return htmlHelper.Partial(templatePath, model).ToHtmlString();
+            } else {
+                var sb = new StringBuilder();
 
-			// To be sure we get the right route, ensure the controller and action are specified.
-			if (!pageLinkValueDictionary.ContainsKey("controller") && routeDataValues.ContainsKey("controller"))
-			{
-				pageLinkValueDictionary.Add("controller", routeDataValues["controller"]);
-			}
-			if (!pageLinkValueDictionary.ContainsKey("action") && routeDataValues.ContainsKey("action"))
-			{
-				pageLinkValueDictionary.Add("action", routeDataValues["action"]);
-			}
+                foreach (var paginationLink in model.PaginationLinks) {
+                    if (paginationLink.Active) {
+                        if (paginationLink.IsCurrent) {
+                            sb.AppendFormat("<span class=\"current\">{0}</span>", paginationLink.DisplayText);
+                        } else if (!paginationLink.PageIndex.HasValue) {
+                            sb.AppendFormat(paginationLink.DisplayText);
+                        } else {
+                            var linkBuilder = new StringBuilder("<a");
 
-			// Fix the dictionary if there are arrays in it.
-			pageLinkValueDictionary = pageLinkValueDictionary.FixListRouteDataValues();
+                            if (pagerOptions.AjaxOptions != null)
+                                foreach (var ajaxOption in pagerOptions.AjaxOptions.ToUnobtrusiveHtmlAttributes())
+                                    linkBuilder.AppendFormat(" {0}=\"{1}\"", ajaxOption.Key, ajaxOption.Value);
 
-			// 'Render' virtual path.
-			var virtualPathForArea = RouteTable.Routes.GetVirtualPathForArea(viewContext.RequestContext, pageLinkValueDictionary);
+                            linkBuilder.AppendFormat(" href=\"{0}\" title=\"{1}\">{2}</a>", paginationLink.Url, paginationLink.DisplayTitle, paginationLink.DisplayText);
 
-			return virtualPathForArea == null ? null : virtualPathForArea.VirtualPath;
-		}
-	}
+                            sb.Append(linkBuilder.ToString());
+                        }
+                    } else {
+                        if (!paginationLink.IsSpacer) {
+                            sb.AppendFormat("<span class=\"disabled\">{0}</span>", paginationLink.DisplayText);
+                        } else {
+                            sb.AppendFormat("<span class=\"spacer\">{0}</span>", paginationLink.DisplayText);
+                        }
+                    }
+                }
+                return sb.ToString();
+            }
+        }
 
-	public class Pager<TModel> : Pager
-	{
-		private HtmlHelper<TModel> htmlHelper;
+        protected virtual string GeneratePageUrl(int pageNumber)
+        {
+            var viewContext = this.htmlHelper.ViewContext;
+            var routeDataValues = viewContext.RequestContext.RouteData.Values;
+            RouteValueDictionary pageLinkValueDictionary;
 
-		public Pager(HtmlHelper<TModel> htmlHelper, int pageSize, int currentPage, int totalItemCount) : base(htmlHelper, pageSize, currentPage, totalItemCount)
-		{
-			this.htmlHelper = htmlHelper;
-		}
+            // Avoid canonical errors when pageNumber is equal to 1.
+            if (pageNumber == 1 && !this.pagerOptions.AlwaysAddFirstPageNumber) {
+                pageLinkValueDictionary = new RouteValueDictionary(this.pagerOptions.RouteValues);
 
-		public Pager<TModel> Options(Action<PagerOptionsBuilder<TModel>> buildOptions)
-		{
-			buildOptions(new PagerOptionsBuilder<TModel>(this.pagerOptions, htmlHelper));
-			return this;
-		}
-	}
+                if (routeDataValues.ContainsKey(this.pagerOptions.PageRouteValueKey)) {
+                    routeDataValues.Remove(this.pagerOptions.PageRouteValueKey);
+                }
+            } else {
+                pageLinkValueDictionary = new RouteValueDictionary(this.pagerOptions.RouteValues) { { this.pagerOptions.PageRouteValueKey, pageNumber } };
+            }
+
+            // To be sure we get the right route, ensure the controller and action are specified.
+            if (!pageLinkValueDictionary.ContainsKey("controller") && routeDataValues.ContainsKey("controller")) {
+                pageLinkValueDictionary.Add("controller", routeDataValues["controller"]);
+            }
+
+            if (!pageLinkValueDictionary.ContainsKey("action") && routeDataValues.ContainsKey("action")) {
+                pageLinkValueDictionary.Add("action", routeDataValues["action"]);
+            }
+
+            // Fix the dictionary if there are arrays in it.
+            pageLinkValueDictionary = pageLinkValueDictionary.FixListRouteDataValues();
+
+            // 'Render' virtual path.
+            var virtualPathForArea = RouteTable.Routes.GetVirtualPathForArea(viewContext.RequestContext, pageLinkValueDictionary);
+
+            return virtualPathForArea == null ? null : virtualPathForArea.VirtualPath;
+        }
+    }
+
+    public class Pager<TModel> : Pager
+    {
+        private HtmlHelper<TModel> htmlHelper;
+
+        public Pager(HtmlHelper<TModel> htmlHelper, int pageSize, int currentPage, int totalItemCount)
+            : base(htmlHelper, pageSize, currentPage, totalItemCount)
+        {
+            this.htmlHelper = htmlHelper;
+        }
+
+        public Pager<TModel> Options(Action<PagerOptionsBuilder<TModel>> buildOptions)
+        {
+            buildOptions(new PagerOptionsBuilder<TModel>(this.pagerOptions, htmlHelper));
+            return this;
+        }
+    }
 }
--- src/MvcPaging/PagerOptions.cs
+++ src/MvcPaging/PagerOptions.cs
@@ -3,61 +3,96 @@ using System.Web.Routing;
 
 namespace MvcPaging
 {
-	public class PagerOptions
-	{
-		public static class DefaultDefaults
-		{
-			public const int MaxNrOfPages = 10;
-			public const string DisplayTemplate = null;
-			public const bool AlwaysAddFirstPageNumber = false;
-			public const string DefaultPageRouteValueKey = "page";
-			public const string PreviousPageText = "«";
-			public const string NextPageText = "»";
-		}
+    public class PagerOptions
+    {
+        public static class DefaultDefaults
+        {
+            public const int MaxNrOfPages = 10;
+            public const string DisplayTemplate = null;
+            public const bool AlwaysAddFirstPageNumber = false;
+            public const string DefaultPageRouteValueKey = "page";
+            public const string PreviousPageText = "«";
+            public const string PreviousPageTitle = "Previous page";
+            public const string NextPageText = "»";
+            public const string NextPageTitle = "Next page";
+            public const string FirstPageText = "<";
+            public const string FirstPageTitle = "First page";
+            public const string LastPageText = ">";
+            public const string LastPageTitle = "Last page";
+            public const bool DisplayFirstAndLastPage = false;
+        }
 
-		/// <summary>
-		/// The static Defaults class allows you to set Pager defaults for the entire application. 
-		/// Set values at application startup.
-		/// </summary>
-		public static class Defaults
-		{
-			public static int MaxNrOfPages = DefaultDefaults.MaxNrOfPages;
-			public static string DisplayTemplate = DefaultDefaults.DisplayTemplate;
-			public static bool AlwaysAddFirstPageNumber = DefaultDefaults.AlwaysAddFirstPageNumber;
-			public static string DefaultPageRouteValueKey = DefaultDefaults.DefaultPageRouteValueKey;
-			public static string PreviousPageText = DefaultDefaults.PreviousPageText;
-			public static string NextPageText = DefaultDefaults.NextPageText;
+        /// <summary>
+        /// The static Defaults class allows you to set Pager defaults for the entire application. 
+        /// Set values at application startup.
+        /// </summary>
+        public static class Defaults
+        {
+            public static int MaxNrOfPages = DefaultDefaults.MaxNrOfPages;
+            public static string DisplayTemplate = DefaultDefaults.DisplayTemplate;
+            public static bool AlwaysAddFirstPageNumber = DefaultDefaults.AlwaysAddFirstPageNumber;
+            public static string DefaultPageRouteValueKey = DefaultDefaults.DefaultPageRouteValueKey;
+            public static string PreviousPageText = DefaultDefaults.PreviousPageText;
+            public static string PreviousPageTitle = DefaultDefaults.PreviousPageTitle;
+            public static string NextPageText = DefaultDefaults.NextPageText;
+            public static string NextPageTitle = DefaultDefaults.NextPageTitle;
+            public static string FirstPageText = DefaultDefaults.FirstPageText;
+            public static string FirstPageTitle = DefaultDefaults.FirstPageTitle;
+            public static string LastPageText = DefaultDefaults.LastPageText;
+            public static string LastPageTitle = DefaultDefaults.LastPageTitle;
+            public static bool DisplayFirstAndLastPage = DefaultDefaults.DisplayFirstAndLastPage;
 
-			public static void Reset()
-			{
-				MaxNrOfPages = DefaultDefaults.MaxNrOfPages;
-				DisplayTemplate = DefaultDefaults.DisplayTemplate;
-				AlwaysAddFirstPageNumber = DefaultDefaults.AlwaysAddFirstPageNumber;
-				DefaultPageRouteValueKey = DefaultDefaults.DefaultPageRouteValueKey;
-				PreviousPageText = DefaultDefaults.PreviousPageText;
-				NextPageText = DefaultDefaults.NextPageText;
-			}
-		}
+            public static void Reset()
+            {
+                MaxNrOfPages = DefaultDefaults.MaxNrOfPages;
+                DisplayTemplate = DefaultDefaults.DisplayTemplate;
+                AlwaysAddFirstPageNumber = DefaultDefaults.AlwaysAddFirstPageNumber;
+                DefaultPageRouteValueKey = DefaultDefaults.DefaultPageRouteValueKey;
+                PreviousPageText = DefaultDefaults.PreviousPageText;
+                PreviousPageTitle = DefaultDefaults.PreviousPageTitle;
+                NextPageText = DefaultDefaults.NextPageText;
+                NextPageTitle = DefaultDefaults.NextPageTitle;
+                FirstPageText = DefaultDefaults.FirstPageText;
+                FirstPageTitle = DefaultDefaults.FirstPageTitle;
+                LastPageText = DefaultDefaults.LastPageText;
+                LastPageTitle = DefaultDefaults.LastPageTitle;
+                DisplayFirstAndLastPage = DefaultDefaults.DisplayFirstAndLastPage;
+            }
+        }
 
-		public RouteValueDictionary RouteValues { get; internal set; }
-		public string DisplayTemplate { get; internal set; }
-		public int MaxNrOfPages { get; internal set; }
-		public AjaxOptions AjaxOptions { get; internal set; }
-		public bool AlwaysAddFirstPageNumber { get; internal set; }
-		public string Action { get; internal set; }
-		public string PageRouteValueKey { get; set; }
-		public string PreviousPageText { get; set; }
-		public string NextPageText { get; set; }
+        public RouteValueDictionary RouteValues { get; internal set; }
+        public string DisplayTemplate { get; internal set; }
+        public int MaxNrOfPages { get; internal set; }
+        public AjaxOptions AjaxOptions { get; internal set; }
+        public bool AlwaysAddFirstPageNumber { get; internal set; }
+        public string Action { get; internal set; }
+        public string PageRouteValueKey { get; set; }
+        public string PreviousPageText { get; set; }
+        public string PreviousPageTitle { get; set; }
+        public string NextPageText { get; set; }
+        public string NextPageTitle { get; set; }
+        public string FirstPageText { get; set; }
+        public string FirstPageTitle { get; set; }
+        public string LastPageText { get; set; }
+        public string LastPageTitle { get; set; }
+        public bool DisplayFirstAndLastPage { get; internal set; }
 
-		public PagerOptions()
-		{
-			RouteValues = new RouteValueDictionary();
-			DisplayTemplate = Defaults.DisplayTemplate;
-			MaxNrOfPages = Defaults.MaxNrOfPages;
-			AlwaysAddFirstPageNumber = Defaults.AlwaysAddFirstPageNumber;
-			PageRouteValueKey = Defaults.DefaultPageRouteValueKey;
-			PreviousPageText = DefaultDefaults.PreviousPageText;
-			NextPageText = DefaultDefaults.NextPageText;
-		}
-	}
+        public PagerOptions()
+        {
+            RouteValues = new RouteValueDictionary();
+            DisplayTemplate = Defaults.DisplayTemplate;
+            MaxNrOfPages = Defaults.MaxNrOfPages;
+            AlwaysAddFirstPageNumber = Defaults.AlwaysAddFirstPageNumber;
+            PageRouteValueKey = Defaults.DefaultPageRouteValueKey;
+            PreviousPageText = DefaultDefaults.PreviousPageText;
+            PreviousPageTitle = DefaultDefaults.PreviousPageTitle;
+            NextPageText = DefaultDefaults.NextPageText;
+            NextPageTitle = DefaultDefaults.NextPageTitle;
+            FirstPageText = DefaultDefaults.FirstPageText;
+            FirstPageTitle = DefaultDefaults.FirstPageTitle;
+            LastPageText = DefaultDefaults.LastPageText;
+            LastPageTitle = DefaultDefaults.LastPageTitle;
+            DisplayFirstAndLastPage = DefaultDefaults.DisplayFirstAndLastPage;
+        }
+    }
 }
